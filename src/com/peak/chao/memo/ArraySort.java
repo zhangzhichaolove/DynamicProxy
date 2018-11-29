6d8690300268d3aca9ccc53072094b104be83af8
zhangzhichao <309324904@qq.com>
2018-11-29 14:48:48 +0800

    add kotlin and add sort simple

---
+++ src/com/peak/chao/memo/ArraySort.java
@@ -0,0 +1,27 @@
+package com.peak.chao.memo;
+
+public class ArraySort implements Runnable {
+    private String num;
+
+    public ArraySort(int num) {
+        this.num = num + "";
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(Integer.parseInt(num));
+            System.err.println(num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {11, 3, 998, 5455, 1, 152, 990};
+        for (int i = 0; i < nums.length; i++) {
+            new Thread(new ArraySort(nums[i])).start();
+        }
+    }
+
+}
---
+++ src/com/peak/chao/memo/NewArraySort.kt
@@ -0,0 +1,47 @@
+package com.peak.chao.memo
+
+
+fun main(args: Array<String>) {
+    val nums = intArrayOf(11, 3, 998, 5455, 1, 152, 990)
+    //nums.sort()
+    //nums.sortDescending()
+    System.err.println(nums.toList())
+
+
+    /******************************************************************************************/
+    var personList: MutableList<Person> = mutableListOf()
+    personList.add(Person("Jim", 12))
+    personList.add(Person("A-Lin", 12))
+    personList.add(Person("Tom", 11))
+    personList.add(Person("Mary", 14))
+    println("----排序前----")
+    personList.forEach(::println)
+    personList.sortBy { it.age }
+    println("----排序后----")
+    personList.forEach(::println)
+    //先根据age 升序排列，若age相同，根据name升序排列
+    personList.sortWith(compareBy({ it.age }, { it.name }))
+    println("----With排序后----")
+    personList.forEach(::println)
+    //sorted 方法返回排序好的list
+    val sorted = personList.sorted()
+    println("----Comparable排序后----")
+    sorted.forEach(::println)
+}
+
+class NewArraySort {
+
+
+}
+
+data class Person(var name: String, var age: Int) : Comparable<Person> {
+
+    override fun compareTo(other: Person): Int {
+        return if (this.age == other.age) {
+            this.name.compareTo(other.name)
+        } else {
+            other.age - this.age
+        }
+    }
+
+}

3d168c73380802933f428f77efa917171cd3cb6b
zhangzhichao <zhangzhichaolove@vip.qq.com>
2019-01-21 11:26:31 +0800

    uodate

---
+++ src/com/peak/chao/other/Main.java
@@ -0,0 +1,15 @@
+package com.peak.chao.other;
+
+import java.lang.reflect.Field;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        TestModel testModel = new TestModel();
+        Field field = testModel.getClass().getDeclaredField("age");
+        field.setAccessible(true);
+        System.err.println(field.get(testModel));
+        field.set(testModel,19);
+        System.err.println(field.get(testModel));
+    }
+}
---
+++ src/com/peak/chao/other/TestModel.java
@@ -0,0 +1,17 @@
+package com.peak.chao.other;
+
+public class TestModel {
+
+
+    private final int age = 18;
+    private String name = "Mr";
+
+
+    @Override
+    public String toString() {
+        return "TestModel{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
